ffed421eb30e6d40886eb0c7424c8c0e5ee96ed9
mhernangilp <mhernangilp@gmail.com>
2023-02-06 12:06:13 +0100

    finished lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,22 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*temp;
-	t_list	*temp2;
-	t_list	*ret;
+	t_list	*new_lst;
+	t_list	*new_node;
 
-	temp = lst;
-	if (!lst)
-		return (ret);
-	ft_lstiter(temp, f);
-	ret = ft_lstnew(temp -> content);
-	temp2 = ret;
-	temp = temp -> next;
-	while (temp)
+	if (!lst || !f || !del)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
 	{
-		temp2 -> next = ft_lstnew(temp -> content);
-		temp = temp -> next;
-		temp2 = temp2 -> next;
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst -> next;
 	}
-	return (ret);
+	return (new_lst);
 }
